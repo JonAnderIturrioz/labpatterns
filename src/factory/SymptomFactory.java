63897b5244fd649d301a813eab60e9c0b7854dbc
JonAnderIturrioz <jiturrioz08@gmail.com>
2024-11-08 22:40:28 +0100

    added hashmap for symptoms in factory

--- src/factory/SymptomFactory.java
+++ src/factory/SymptomFactory.java
@@ -12,6 +12,7 @@ import domain.Symptom;
 
 public class SymptomFactory {
 	
+	//Hashmap to keep symptom instance list
 	private static Map<String, Symptom> symptomList = new HashMap<>();
 	
 	public static Symptom createSymptom(String symptomName) {
@@ -19,12 +20,12 @@ public class SymptomFactory {
 	    List<Double> index5 = Arrays.asList(87.9, 67.7, 38.1, 33.4);
 	    List<String> impact3 = Arrays.asList("disnea", "dolor de garganta", "cefalea","mialgia","escalofrios");
 	    List<Double> index3 = Arrays.asList(18.6, 13.9, 13.6, 14.8, 11.4);
-	    List<String> impact1 = Arrays.asList("mareos","nauseas", "vómitos", "congestión nasal","diarrea","hemoptisis","congestion conjuntival");
+	    List<String> impact1 = Arrays.asList("mareos","nauseas", "vómitos", "congestión nasal","diarrea","hemoptisis","congestion conjuntival"); //symptom mareos added 
 	    List<Double> index1 = Arrays.asList(6.0, 5.0, 4.8, 3.7, 0.9, 0.8);
 	    
 	    List<String> digestiveSymptom=Arrays.asList("nauseas", "vómitos","diarrea");
 	    List<String> neuroMuscularSymptom=Arrays.asList("fiebre", "astenia", "cefalea", "mialgia","escalofrios", "mareos");
-	    List<String> respiratorySymptom=Arrays.asList("tos seca","expectoracion","disnea","dolor de garganta", "congestión nasal","hemoptisis","congestion conjuntival");
+	    List<String> respiratorySymptom=Arrays.asList("tos seca","expectoracion","disnea","dolor de garganta", "congestión nasal","hemoptisis","congestion conjuntival"); //symptom mareos added
 
 	 // Check if instance already exists in the map
         if (symptomList.containsKey(symptomName)) {
@@ -43,6 +44,7 @@ public class SymptomFactory {
 	    	if (neuroMuscularSymptom.contains(symptomName)) s = new NeuroMuscularSymptom(symptomName,(int)index, impact);
 	    	if (respiratorySymptom.contains(symptomName)) s = new RespiratorySymptom(symptomName,(int)index, impact);
 	    	
+	    	//add new symptom to list
 	    	if (s != null) {
 	    		symptomList.put(symptomName, s);
 	    		return s;
